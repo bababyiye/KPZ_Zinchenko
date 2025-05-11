dd15d884959808d636541d98863be6f196e3b4d4
Maxim <ipz235_zmv@student.ztu.edu.ua>
2025-05-11 17:49:17 +0300

    додавання першої лаби наново на окрему гілку lab1

---
+++ KPZ1/GlobalClass/ZooInventory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalClass
+{
+
+    public class ZooInventory
+    {
+        public List<Animal> Animals { get; set; } = new List<Animal>();
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+        public List<Enclosure> Enclosures { get; set; } = new List<Enclosure>();
+
+
+        public void DisplayInventory()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("--- Інвентаризація зоопарку ---\n");
+            Console.WriteLine("Тварини:");
+            foreach (var animal in Animals)
+            {
+                animal.DisplayInfo();
+            }
+            Console.WriteLine("\n");
+
+
+            Console.WriteLine("Працівники:");
+            foreach (var employee in Employees)
+            {
+                employee.DisplayEmployeeInfo();
+            }
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Вольєри:");
+            foreach(var enclosure in Enclosures)
+            {
+                enclosure.DisplayEnclosureInfo();
+            }
+        }
+    }
+}
